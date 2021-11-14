c58a5f404ba24e8a0badf7a2af5312d1f18a236a
Your Name <you@example.com>
2021-11-14 10:54:51 +0800

    init

--- 5.loader/webpack.config.js
+++ 5.loader/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devtool: false,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
@@ -11,7 +12,8 @@ module.exports = {
         //配置别名
         alias: {
             'inline1-loader': path.resolve(__dirname, 'loaders', 'inline1-loader.js'),
-            'inline2-loader': path.resolve(__dirname, 'loaders', 'inline2-loader.js')
+            'inline2-loader': path.resolve(__dirname, 'loaders', 'inline2-loader.js'),
+            'babel-loader': path.resolve(__dirname, 'loaders', 'babel-loader')
         },
         //配置去哪些目录里找loader
         modules: ['node_modules', path.resolve(__dirname, 'loaders')]
@@ -20,29 +22,13 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use: [
-                    //在配置里直接这样配置的话，类型就是normal
-                    path.resolve(__dirname, 'loaders', 'normal1-loader.js'),
-                    path.resolve(__dirname, 'loaders', 'normal2-loader.js')
-                ]
-            },
-            {
-                test: /\.js$/,
-                enforce: 'post',
-                use: [
-                    //如果在配置的时候添加了enforce=post参数，那么这个loader就是是post-loader
-                    path.resolve(__dirname, 'loaders', 'post1-loader.js'),
-                    path.resolve(__dirname, 'loaders', 'post2-loader.js')
-                ]
-            },
-            {
-                test: /\.js$/,
-                enforce: 'pre',
-                use: [
-                    //如果在配置的时候添加了enforce=post参数，那么这个loader就是是post-loader
-                    path.resolve(__dirname, 'loaders', 'pre1-loader.js'),
-                    path.resolve(__dirname, 'loaders', 'pre2-loader.js')
-                ]
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: []
+                    }
+                }
             }
         ]
     }
