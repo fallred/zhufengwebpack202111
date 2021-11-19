96cf0358fe1e34da8c890b73bb59893e4672858a
Your Name <you@example.com>
2021-11-19 21:01:52 +0800

    init

--- 7.plugin/dist/main.js
+++ 7.plugin/dist/main.js
@@ -1,8 +1,27 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-
-/******/ })()
-;
+(() => {
+  var modules = ({
+    "?4105":
+      ((module) => {
+        "use strict";
+        module.exports = _;
+      })
+  });
+  var cache = {};
+  function require(moduleId) {
+    var cachedModule = cache[moduleId];
+    if (cachedModule !== undefined) {
+      return cachedModule.exports;
+    }
+    var module = cache[moduleId] = {
+      exports: {}
+    };
+    modules[moduleId](module, module.exports, require);
+    return module.exports;
+  }
+  var exports = {};
+  (() => {
+    let _ = require("?4105");
+    console.log(_);
+  })();
+})()
+  ;
